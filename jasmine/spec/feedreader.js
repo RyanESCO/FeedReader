cee8d8f04fec78c49b36e991e3d8294bb1893d9b
rcdymock <ryan.dymock@escocorp.com>
2015-11-11 09:56:48 -0800

    code improvements based on feedback

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,82 +1,82 @@
-
+//wrapped in $() so it fully waits for DOM to load before executing
 $(function() {
-    describe('RSS Feeds', function() {
+  //Test Suite "RSS Feeds" checks the feed related elements of the page
+  describe('RSS Feeds', function() {
 
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
-        
-        it('has URL defined', function () {
-            for (var i = 0; i < allFeeds.length; i++) {
-              var feed = allFeeds[i];
-              expect(feed.url).toBeDefined();
-              expect(feed.url).not.toBe("");
-          }
-        });
-         
-          it('has name defined', function () {
-            for (var i = 0; i < allFeeds.length; i++) {
-              var feed = allFeeds[i];
-              expect(feed.name).toBeDefined();
-              expect(feed.name).not.toBe("");
-           }
-          });
+    //check that there are feeds defined (in allFeeds array)
+    it('are defined', function() {
+      expect(allFeeds).toBeDefined();
+      expect(allFeeds.length).not.toBe(0);
     });
 
-    describe('The Menu', function() {
-         
-         it('hidden when first loaded', function () {
-              expect($('body').hasClass('menu-hidden')).toBe(true);
-        });
-          
-       it('opens and closes when clicked', function () {
-          expect($('body').hasClass('menu-hidden')).toBe(true);
-          $('.menu-icon-link').trigger( "click" );
-          expect($('body').hasClass('menu-hidden')).toBe(false);
-          $('.menu-icon-link').trigger( "click" );
-          expect($('body').hasClass('menu-hidden')).toBe(true);
-        });
-     });     
-      
-     describe('Initial Entries', function() {  
-       
-       beforeEach(function(done){
-         loadFeed(0, function(){
-           done(); 
-         });
-         
-         
-       });
-       
-       it('should have an entry in feed after load',function(done){
-         
-         expect($('.feed .entry').length>0).toBe(true);
-         done();
-       });
-         
-     });
+    //check that each feed as an associated URL
+    it('has URL defined', function(){      
+      allFeeds.forEach(function(element){
+        expect(element.url).toBeDefined();
+        expect(element.url).not.toBe('');
+      });
+    });
     
-describe('New Feed Selection', function(){
-  var firstHeader, secondHeader;
+    //check that each feed has a name 
+    it('has name defined', function() {
+      allFeeds.forEach(function(element){
+        expect(element.name).toBeDefined();
+        expect(element.v).not.toBe('');
+      });
+    });
+  });
+  //Test Suite "The Menu" checks the functions of the menu
+  describe('The Menu', function() {
+    var $body = $('body');
+    var $menuButton = $('.menu-icon-link');
+    
+    //check that the menu is hidden when the page is first loaded
+    it('hidden when first loaded', function() {
+      expect($body.hasClass('menu-hidden')).toBe(true);
+    });
+    
+    //check for the operation of the menu button, does it open and close when clicked
+    it('opens and closes when clicked', function() {
+      expect($body .hasClass('menu-hidden')).toBe(true);
+      $menuButton .trigger('click');
+      expect($body .hasClass('menu-hidden')).toBe(false);
+      $menuButton .trigger('click');
+      expect($body .hasClass('menu-hidden')).toBe(true);
+    });
+  });
   
-    beforeEach(function(done){
-        loadFeed(0, function(){
-            firstHeader = $('.feed').find("h2").text();
-            console.log(firstHeader);
-            done();
-        });
-        
+  //Test Suite "Initial Entries" checks the UI of the feed entries
+  describe('Initial Entries', function() {
+    //for these tests to be useful, a successful load of the rss feeds needs to completed
+    beforeEach(function(done) {
+      loadFeed(0, done);
     });
 
-    it('changes the content', function(done){
-         loadFeed(1, function() {
-             secondHeader = $('.feed').find("h2").text();
-             console.log(secondHeader);
-         });
-        expect(firstHeader).not.toEqual(secondHeader);
-        done();
+    it('should have an entry in feed after load', function() {
+      expect($('.feed .entry').length > 0).toBe(true);
     });
-});
+
+  });
+
+  describe('New Feed Selection', function() {
+    var firstHeader, secondHeader;
+    var $feed = $('.feed');
+
+    beforeEach(function(done) {
+      loadFeed(0, function() {
+        firstHeader = $feed.find('h2').text();
+        done();
+      });
+
+    });
+
+    it('changes the content', function() {
+      loadFeed(1, function() {
+        secondHeader = $feed.find('h2').text();
+        console.log(secondHeader);
+      });
+      expect(firstHeader).not.toEqual(secondHeader);
+    });
+  });
 
 }());
